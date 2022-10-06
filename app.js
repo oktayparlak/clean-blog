33cbf2935a623d0e68f9475dd9555f47b4b92393
oktayparlak <oktay.parlak.12@hotmail.com>
2022-10-06 16:36:58 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Blog Title',
+    description: 'Blog description',
+  };
+  res.send(photo);
+});
